135481e64a991c6085052566ac9fbc6430a05074
高禎駿 <b02705050@ntu.edu.tw>
2017-12-22 21:06:04 +0800

    f

--- day21-web.py
+++ day21-web.py
@@ -7,4 +7,9 @@ my_url = requests.get(url)
 #print(my_url.text)
 
 my_url_soup = BeautifulSoup(my_url.text,'html.parser')
-print(my_url_soup)
+#print(my_url_soup)
+
+#print(my_url_soup.prettify())
+
+for html_tag in my_url_soup.findAll('p'):
+    print(html_tag)
