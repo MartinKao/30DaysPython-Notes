0815e4886a6643c9dde5574df7e6b423074196a3
高禎駿 <b02705050@ntu.edu.tw>
2017-12-25 08:55:36 +0800

    j

--- day25.py
+++ day25.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
@@ -10,6 +11,7 @@ with open("normal.txt","w+") as normal_txt:
         normal_txt.write(str(row))
 
 '''
+'''
 #driver = webdriver.Firefox()
 driver = webdriver.Chrome()
 driver.get("http://www.chrisburkard.com/Shop")
@@ -19,3 +21,25 @@ sel_soup = BeautifulSoup(html,"html.parser")
 with open("driver.txt","w+") as driver_txt:
     for row in sel_soup:
         driver_txt.write(str(row))
+
+'''
+
+images = []
+for i in sel_soup.findAll("img"):
+    src = i["src"]
+    images.append(src)
+
+current_path = os.getcwd()
+for img in images:
+    try:
+        file_name = os.path.basename(img)
+        img_r = requests.get(img, stream=True)#打開 images 的網址連結
+        new_path = os.path.join(current_path,"images",file_name)#目標是將圖片存到 images 這個資料夾內。
+        with open(new_path,"wb") as output_file:
+            shutil.copyfileobj(img_r,raw,output_file)#將網路上的 images 內容複製到目標檔案。
+        del img_r#刪除已開啟的圖片連結
+    except:
+        pass
+
+
+
